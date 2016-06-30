baf3c871c3030de74707705ee0952b07fac5690c
Fabricio Catae <fcatae@microsoft.com>
2016-06-30 12:46:56 -0300

    Add test project

---
+++ test/Test-DataDriver/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test_DataDriver
+{
+    public class Class1
+    {
+        [Fact]
+        public void Teste1()
+        {
+            System.Diagnostics.Debug.WriteLine("Hello world");
+        }
+
+        [Fact]
+        public void Somar()
+        {
+            Assert.Equal( 1 + 1 , 2 );
+        }
+
+        [Fact]
+        void Teste2()
+        {
+        }
+
+    }
+}
